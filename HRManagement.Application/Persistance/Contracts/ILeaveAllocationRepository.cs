1846709c80c30a77a3415d073e68b8ec49ac13cb
Pedro Morante <pedro.morante@outlook.com>
2023-01-17 15:24:09 -0500

    Add Repositories

---
+++ HRManagement.Application/Persistance/Contracts/ILeaveAllocationRepository.cs
@@ -0,0 +1,13 @@
+﻿using HRManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRManagement.Application.Persistance.Contracts
+{
+    internal interface ILeaveAllocationRepository : IGenericRepository<LeaveAllocation>
+    {
+    }
+}
