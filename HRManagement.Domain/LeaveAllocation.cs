f4284b3c9a7b67cc184557960d9725019f48b8e3
Pedro Morante <pedro.morante@outlook.com>
2023-01-17 15:09:24 -0500

    initial commit

---
+++ HRManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRManagement.Domain.Common;
+
+namespace HRManagement.Domain
+{
+    public class LeaveAllocation : BaseDomainEntity
+    {
+        public int NumberOfDays { get; set; }
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
+        public string EmployeeId { get; set; }
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+    }
+}
---
+++ HRManagement.Domain/LeaveType.cs
@@ -0,0 +1,11 @@
+﻿using HRManagement.Domain.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace HRManagement.Domain
+{
+    public class LeaveType : BaseDomainEntity
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+    }
+}
